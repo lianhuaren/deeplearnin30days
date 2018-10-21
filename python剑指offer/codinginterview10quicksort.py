147f53ed974665c9c9e73eea7f92740e5b02be51
lianhuaren <lianhuaren@gmail.com>
2018-10-22 07:35:52 +0800

    quicksort

--- python剑指offer/codinginterview10quicksort.py
+++ python剑指offer/codinginterview10quicksort.py
@@ -8,21 +8,26 @@ class Solution:
         # 快速排序
         if left >= right:
             return lists
+
         key = lists[left]
         low = left
         high = right
+
+        print "before One iteration key:",key,lists
+
         while left < right:
             while left < right and lists[right] >= key:
                 right -= 1
             lists[left] = lists[right]
             
-            print ("left:",left,"right:",right)
+            print ("change left=right left:",left,"right:",right)
             while left < right and lists[left] <= key:
                 left += 1
             lists[right] = lists[left]
+            print ("change right=left left:",left,"right:",right)
         lists[right] = key
 
-        print ("key:",key ,lists)
+        print ("after One iteration key:",key ,lists)
         print ("-----------------------")
         self.quick_sort(lists, low, left - 1)
         self.quick_sort(lists, left + 1, high)
